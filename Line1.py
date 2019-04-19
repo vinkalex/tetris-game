ad792b2a6f41a809dd0d33264101c5c65d28f6b3
vinkalex <48713822+vinkalex@users.noreply.github.com>
2019-04-19 20:31:29 +0300

    Add files via upload

--- Line1.py
+++ Line1.py
@@ -3,17 +3,19 @@ import pygame
 
 
 class Line(Piece):
+    """Inherited class Line"""
     def __init__(self):
+        """Method of initialization of inherited class uses method of initialization of parent class
+        Field rows is assigned with 4, field columns is assigned with 1, field types if assigned with "line",
+        field data is assigned with corresponding matrix,  field board is assigned with 380,
+        field color is assigned with (139, 0, 139)"""
         Piece.__init__(self, 4, 1, "line", [[1] * 4 for i in range(1)], 380, (139, 0, 139))
 
     def print(self, x_coord, y_coord, side, window):
+        """Method of printing the figure
+        Figure should look like this:
+           XXXX    """
         pygame.draw.rect(window, self.colors, (x_coord, y_coord, side, side))
         pygame.draw.rect(window, self.colors, (x_coord + 20, y_coord, side, side))
         pygame.draw.rect(window, self.colors, (x_coord + 40, y_coord, side, side))
         pygame.draw.rect(window, self.colors, (x_coord + 60, y_coord, side, side))
-
-    def type(self):
-        return self.types
-
-    def boardd(self):
-        return self.board
