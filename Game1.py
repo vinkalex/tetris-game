280e516f76d780b5fa899799b9c4331fad7fa22b
vinkalex <48713822+vinkalex@users.noreply.github.com>
2019-04-19 20:32:33 +0300

    Add files via upload

--- Game1.py
+++ Game1.py
@@ -7,7 +7,7 @@ import random
 
 
 class Game():
-    """Класс - реализация игровой механики"""
+    """Class - implementation of game mechanics"""
     lst = [[0] * 16 for i in range(11)]
     position_x = 0
     position_y = 0
@@ -19,16 +19,16 @@ class Game():
     score = 0
 
     def __init__(self):
-        """Метод - инициализация класса игра"""
+        """Method - initialization of class game"""
         pass
 
     def check_the_lines(self, width, length):
-        """Метод
-        Входные данные - принимает размеры матрицы игрового поля width и length(элемент равен 1 если клетка занята,
-        0 если нет)(числа)
-        Проверяет, есть ли в матрице строки, заполненные 1
-        Возвращает номер строки (check - число) заполненной 1(если таких строк
-        несколько, возвращает номер той, которая выше), если такой строки нет - возвращает -1"""
+        """Method
+        Input data - takes sizes of matrix of the game field - width and length
+        (element's value is 1 if the cell is not empty, 0 else)(numeric)
+        Checks, whether the row in the matrix is ​​filled with 1
+        Returns line number (check - numeric) which is filled with 1( if it contains several lines of that type,
+        returns line number of line which is above), if there is no lines like this - returns -1"""
         check = -1
         for i in range(0, length - 2):
             flag = 1
@@ -40,16 +40,16 @@ class Game():
         return check
 
     def choose_the_figure(self):
-        """Метод
-        Входные данные - нет
-        Выбирает рандомную фигуру их списка классов заданных фигур"""
+        """Method
+        Input data - none
+        Chooses random figure among the list of available figures"""
         list_of_classes = [Alone, Quatro, Tetra, Line]
         self.figure = random.choice(list_of_classes)()
 
     def set_the_place_of_figure(self):
-        """Метод
-        Входные данные - нет
-        Рандомно задает координату по х в зависимости от типа заданной фигуры"""
+        """Method
+        Input data - none
+        Randomly sets x coordinate depending of the type of given figure"""
         self.position_x = 0
         if self.figure.type() == "alone":
             self.position_x = random.choice([150, 170, 190, 210, 230, 250, 270, 290, 310, 330])
@@ -61,11 +61,11 @@ class Game():
             self.position_x = random.choice([150, 170, 190, 210, 230, 250, 270])
 
     def check_the_condition(self):
-        """Метод
-        Входные данные - нет
-        В зависимости от типа фигуры изменяет переменную condition(число) - проверяет, пуста ли строка под фигурой
-        В результате в переменной condition будет 0 - есл строка свободна, либо число - отичное от 0 -
-        значит строка под фигурой не свободна для ее перемещения вниз"""
+        """Method
+        Input data - none
+        Depending on the type of figure changes variable condition(numeric) - checks, whether line under figure is empty
+        As a result in variable condition will be 0 - if the line is available or number which is different from 0 -
+        it means that line under figure is not available for moving the figure down"""
         self.condition = 0
         if self.figure.type() == "line":
             self.condition = self.lst[(self.position_x - 150) // 20][((self.position_y + 20) - 100) // 20] + \
@@ -83,16 +83,16 @@ class Game():
                              self.lst[(self.position_x - 150 + 40) // 20][((self.position_y + 40) - 100) // 20]
 
     def fill_the_screen(self, win):
-        """Метод
-        Входные данные - окно, в котором будем работать
-        Заливает окно фоновым цветом"""
+        """Method
+        Входные данные - window in which we will draw
+        Fills the window with background color"""
         win.fill((0, 0, 0))
 
     def print_the_screen_and_figure(self, win):
-        """Метод
-        Входные данные - окно, в котором будем работать
-        Метод отрисовывает в окне весь графический интерфейс - клеточки, окно со счетом игры, игровое поле,
-        фигуру в данный момент времени"""
+        """Method
+        Input data - window in which we will draw
+        The method draws the entire graphical interface in the window - cells, window with game score, game field,
+        figure in current period of time"""
         font = pygame.font.Font(None, 25)
         text = font.render("TOTAL SCORE:", True, (255, 255, 255))
         win.blit(text, [190, 435])
@@ -124,9 +124,9 @@ class Game():
                                                         self.side_of_piece - 1, self.side_of_piece - 1))
 
     def keyboard_event_handling(self):
-        """Метод
-        Входные данные - нет
-        Обработка действий с клавиатурой"""
+        """Method
+        Input data - none
+        Handling keyboard actions"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.Run = False
@@ -147,15 +147,15 @@ class Game():
 
     def move(self):
         """Метод
-        Входные данные - нет
-        Изменяет координату фигуры по y - сдвигает всю фигуру на клетку вниз"""
+        Input data - none
+        Changes x and y coordinate - shifts the whole figure down"""
         self.position_y += 20
 
     def stop_the_figure(self):
         """Метод
-        Входные данные - нет
-        Метод - обработка остановки фигуры, изменяет матрицу игрового поля в соответствии с данным конечным
-        положением фигуры(заполняет элементы текущего положения единицами"""
+        Input data - none
+        Method - handling of stop process of the figure, changes game field matrix according to final figure position
+        (filling matrix elements with 1)"""
         self.position_y -= 20
 
         if self.figure.type() == "alone":
@@ -178,9 +178,9 @@ class Game():
 
     def delete_full_lines(self):
         """Метод
-        Входные данные - нет
-        Метод удаляет все строки матрицы игрового поля, заполненные единицами, если таковые есть
-        в соответствии с этим меняет счет игры"""
+        Input data - none
+        Deletes all the lines of game field matrix which is filled with 1 if these lines exists,
+        and according to this changes game score"""
         if self.check_the_lines(11, 16) != -1:
             i = self.check_the_lines(11, 16)
             while i >= 1:
@@ -193,9 +193,9 @@ class Game():
             self.score += 10
 
     def print_the_figures(self, win):
-        """Метод
-        Входные данные - окно, в котором будем работать
-        Метод отрисовывает заполненное на данный момент игровое поле"""
+        """Method
+        Input data - window in which we will draw
+        Draws current game field (with the help of game field matrix)"""
         for i in range(11):
             for j in range(16):
                 if self.lst[i][j] == 1:
@@ -203,12 +203,12 @@ class Game():
         self.position_y = 80
 
     def run(self):
-        """Метод, запускающий игру
-        Входные данные - нет
-        использует все вышеперечисленные методы, описывает игровую механику"""
+        """Method that runs the game
+        Input data - none
+        Uses all the methods above, describes the game mechanics"""
         pygame.init()
         window = pygame.display.set_mode((500, 500))
-        pygame.display.set_caption("Cubes Game")
+        pygame.display.set_caption("TETRIS GAME")
 
         self.choose_the_figure()
         self.set_the_place_of_figure()
